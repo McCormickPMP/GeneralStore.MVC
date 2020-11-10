901c9377335aa14772b279a3df4c6bfde3c34428
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-11-10 10:15:49 -0500

    Added NavBar updates and Product Cont updates for Post, Delete and Details

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using GeneralStore.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,14 +14,16 @@ namespace GeneralStore.MVC.Controllers
         // GET: Product/Index
         public ActionResult Index()
         {
-            return View();
+            List<Product> productList = _db.Products.ToList();
+            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();
+            return View(orderedList);
         }
         // Get: Product/Create
         public ActionResult Create()
         {
             return View();
         }
-        // POST: Product
+        // POST: Product/Create
         [HttpPost]
         public ActionResult Create(Product product)
         {
@@ -31,6 +34,73 @@ namespace GeneralStore.MVC.Controllers
                 return RedirectToAction("Index");
             }
 
+            return View(product);
+        }
+        // GET : Product/Delete/{id}
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        //POST : Product/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET  Edit/Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        // POST : Edit//Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+        //GET : Details//Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
             return View(product);
         }
     }
