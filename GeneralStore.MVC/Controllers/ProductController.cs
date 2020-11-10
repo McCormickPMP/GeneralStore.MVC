84330858a1bec910aefa5525fe441ae355da9ae8
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-11-10 09:19:04 -0500

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Get: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
